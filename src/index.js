36fb1bae1d7fc8dc97123bbf8b0a43c87d5f9afd
Rui <464362353@qq.com>
2017-09-19 13:10:13 +0800

    TodoList初始架构

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 import './index.css';
-//import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-
-
-ReactDOM.render(<h1>Hi,World</h1>, document.getElementById('root2'));
-registerServiceWorker();
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
